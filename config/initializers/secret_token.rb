f120ce92df4133b314e5a163e24f08de2613e14d
Matt Buck <github@matthewbuck.com>
2013-10-13 14:56:41 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlurredPress::Application.config.secret_key_base = 'eccd1a67dfaf3945a1712e2a8e630501da109748ffe0fae0308e8f66a02e0deac4443c9fbc6d58ec2dde5b751a87856cc557062defa1ea8cfca147710252b0df'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlurredPress::Application.config.session_store :cookie_store, key: '_blurred-press_session'
